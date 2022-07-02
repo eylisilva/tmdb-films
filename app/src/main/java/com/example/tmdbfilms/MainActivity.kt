0944f907e071ded613eb789a8a3e615cd0b0af22
lienyu123 <lienyu123@gmail.com>
2022-07-02 20:10:43 +0800

    feat: 1.Splash Screen 2.remove the action bar and the status bar

--- app/src/main/java/com/example/tmdbfilms/MainActivity.kt
+++ app/src/main/java/com/example/tmdbfilms/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.tmdbfilms
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
